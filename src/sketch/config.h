334386af40a1dd5c65626430e00e285375fd7de4
Arun CS <aruncs31ss@gmail.com>
2024-10-01 05:13:13 +0530

    initial commit

---
+++ src/sketch/config.h
@@ -0,0 +1,11 @@
+#ifndef __CONFIG_H_
+#define __CONFIG_H_
+#include <stdbool.h>
+
+typedef struct Data {
+  double rain_volume;
+  float battery_voltage;
+  bool led_relayState;
+} Data;
+
+#endif //!__CONFIG_H_
